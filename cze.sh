5002ce3a0cc7643ba48ebf555f9a6c213ef335dd
DIALECTICALEGODEMIGOD <@ .net>
2024-01-25 22:10:19 +0000

    31

--- cze.sh
+++ cze.sh
@@ -6,17 +6,18 @@ api_get() {
 	curl -s -H "Authorization: token $github_token" "$@"
 }
 find_job() {
-	api_get "$base_uri"in_progress
-	api_get "$base_uri"queued
+	api_get "$base_uri"in_progress;
+	api_get "$base_uri"queued;
 }
 i="$(cat cunny)"
 echo $(( $i + 1 )) > cunny
 git add cunny; git commit -m $(($i+1)); git pull; git push
 sleep 2
 # Get the logs URL for the most recent workflow run
-html_url=$(find_job \
-  | grep -m1 jobs_url | awk -F'"' '{print $4}' | xargs \
-  curl -s -H "Authorization: token $github_token" \
-  | grep -m1 html_url | awk -F'"' '{print $4}')
+# html_url=$(
+find_job  | grep -m1 jobs_url 
+# | awk -F'"' '{print $4}' | xargs \
+#  curl -s -H "Authorization: token $github_token" \
+#  | grep -m1 html_url | awk -F'"' '{print $4}')
 
 termux-open-url $html_url
