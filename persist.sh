d51f27be0790061801b6f8f3f63cd9cf05d8e9d9
SCHIZOAFFECTIVEPOSTMODERNISTDISORDER:DDDDDD <@ .net>
2024-01-27 22:00:50 +0000

    iDone v2

--- persist.sh
+++ persist.sh
@@ -6,11 +6,6 @@ then
 	git add persist
 	git commit -m "persist"; git push;
 else
-	if ! [ $(id -u) -eq 0 ]
-	then
-		echo "Run as root tard"
-		exit 1
-	fi
 encryp="-----BEGIN AGE ENCRYPTED FILE-----
 YWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IHNjcnlwdCBzbWQ5Y1ZPb3FMeGtMMnFE
 UXJSYWhBIDE4CmxKR1F4cVVYQnlHdXdkSURvKzFKYkpWMis0V0Y4amVaNDlPc3pY
@@ -21,9 +16,11 @@ AJV43XOlHKdNE9FWZFRkDn9mBZSe9/fYrN8kTNwgoQMUqiNMcZmeqYl3qq0MsvRa
 KbhXjz1sPtRIS8vkm1Fa9o/jNinikY029w/AcxdJ57znfUlB70jzZ9UUbMFZfQ23
 l3OpRgsIjw==
 -----END AGE ENCRYPTED FILE-----"
-	apt install age
+	sudo apt install age || { echo "Run as root tard"; exit 1; }
 	cd "$(dirname $0)"
+	mv "$0" ../ 
 	find ! -name . -prune -exec rm -rf \{\} \+
+	trap 'mv ../persist.sh ./' INT TERM HUP EXIT
 	d="$(pwd)"
 	# clones git repo
 	$(echo "$encryp" | age -d) ./
