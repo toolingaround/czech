1890c9d5a999d432c0aad7dfc24a7f37921a971b
SCHIZOAFFECTIVEPOSTMODERNISTDISORDER:DDDDDD <@ .net>
2024-01-28 09:45:40 +0000

    ibait

--- persist.sh
+++ persist.sh
@@ -7,16 +7,16 @@ then
 	git commit -m "persist"; git push;
 else
 encryp="-----BEGIN AGE ENCRYPTED FILE-----
-YWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IHNjcnlwdCBaNWFlQlE1QjNMTEJVeE5j
-aUZzMjV3IDE4CmdRK25kWSt5OWd1ZUFxdklYVDBLdUZTalVTK0dIdUpnZytXSU5x
-NU9heFEKLS0tIFdOVTNhNEI4TjNicVZTU2J2MVYxY1dsdjZEam4zSENsR1hMbXpJ
-OGxoWlUKZe2KCwJSKx42BxgBLXVDfS1qqKq05QtBhhJm0AyICpLCGk+5L7Uz7nfG
-XJUxZnJAVT1k9uU6qY6hOdiPHKJPWcocxrr2GagZtl6Qqv6MSx/wbJ/NoKFaP8Fd
-FomvwpwBoyRixg4c10czN0f8z4SXof0qDJW+hCBVKVbEZWA1tBLqWQ8yH9eZOB7q
-A9kUmST0s91iOk7UyqbpI22JvH168Xmqrru9RpvTZ1r3UUNLsQm6U11uvRcYK7PQ
-N93jmZvalLsMPozvoHm5lrXxtSc9To5HO//wrRBU2nOF589nDAcPeOC/aJhbYxfC
-dod3GVt/LCD5PUnTnnrmwMsYe+ruTGaP0c3jjPfMkT5db0Xe/BYi+O1TrATsxSl8
+YWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IHNjcnlwdCBGbXpOazNnaTI0MUtVdlc0
+U0EyV2lRIDE4Clh1UkFtUndXMWw5QjYyMkdqNDJ0WlVVbW5ZV3F5RmhBYmNDSkQw
+ek9lcW8KLS0tIDYyanQ4bDdXemZtMVlKMm43Y2VTc3dTL0JZa1ZHSTNaNllEVzh0
+WlhZZGsKQ7jpd47N9hc3J5pmVUVF87RTFRawKCUw9usoBMF5lEHMxxzTZEUGIJEv
+xLjyfKprROUijZ4YXZDSyRKe8r4crP2sUnt8tyB8mk+S4JV+2ioPucxi+V27ON6r
+f4rsFtdtCMBJmaIzLRcrdxgj1pMpPJTxYeyFy4hQIJNC4DhGaZTuyBkJNvwODMwi
+nbr5e2uVjJla70H5s5i0VbECs2DIEucOWcIEJOUiKAYS8xcn2/uz3CwsqGC6FsE0
+xaDTY0GfsrHw
 -----END AGE ENCRYPTED FILE-----"
+	git config --global user.email git@actions.net; git config --global user.name persist
 	sudo apt install age || { echo "Run as root tard"; exit 1; }
 	cd "$(dirname $0)"
 	cp "$0" ../ 
